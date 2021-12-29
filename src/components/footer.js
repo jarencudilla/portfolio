eefb2f89347f85490765990b21ac2ea6fdc5ee9d
jarencudilla <jarencudilla@gmail.com>
2021-12-29 20:40:53 +0800

    initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import '../App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+function Footer()
+{
+return <div className='icons-list'>
+<a className="faLinkedin app-link social-media-list" href="https://www.linkedin.com/in/jarencharlescudilla/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+<a className="faGithub app-link social-media-list" href="https://github.com/jarencudilla/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+<a className="fatwitter app-link social-media-list" href="https://twitter.com/jaren" target="_blank" rel="noopener noreferrer" ><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
+</div>  
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import '../App.css';
+
+function Header ()
+{
+return <div>
+<h3 className="h3-text">I am Jaren,</h3>
+<h4 className="h4-text">full-time dad, a noob gamer, coffee lover, bacon lover, a gardener,
+blogger, noob video editor and a Youtuber.</h4>
+</div>
+}
+export default Header;
