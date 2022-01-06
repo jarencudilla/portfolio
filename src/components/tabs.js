6194772a2df7ae1bd46f062df55d56733f418a63
Jaren Cudilla <jarencudilla@gmail.com>
2022-01-06 15:38:59 +0800

    Update tabs.js

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -27,21 +27,14 @@ function HTabs()
     </TabPanel>
     <TabPanel>
       <div> 
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5"></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
+        Contact Form
+
+        <label>Name</label>
+        <label>Email</label>
+
+        <label>Message</label>
+
+        <button>Submit</button> 
       </div>
     </TabPanel>
   </Tabs></div>
